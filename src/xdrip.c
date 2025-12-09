ac2b76f7fa2e2b9068c1c574bdf269ffeb395a32
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2025-12-09 06:52:05 +0100

    Add 100x144 test graph for TEST_MODE

--- src/xdrip.c
+++ src/xdrip.c
@@ -2060,6 +2060,14 @@ void window_load_cgm(Window *window_cgm) {
     layer_set_update_proc(bitmap_layer_get_layer(bg_trend_layer),bitmapLayerUpdate);
 #endif
 
+#ifdef TEST_MODE
+    // Load test graph image for quick layout testing with glucose graph in emulator
+    bg_trend_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TEST_GRAPH);
+    if (bg_trend_bitmap != NULL) {
+        bitmap_layer_set_bitmap(bg_trend_layer, bg_trend_bitmap);
+    }
+#endif
+
     // DELTA BG
 #ifdef DEBUG_LEVEL
     APP_LOG(APP_LOG_LEVEL_INFO, "Creating Delta BG Text layer");
